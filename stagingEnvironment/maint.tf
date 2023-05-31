3855866c387ba87530186c6e86290a816f05e4cb
obabaldbiyat <127845320+obabaldbiyat@users.noreply.github.com>
2023-05-31 12:12:37 +0200

    Add files via upload

---
+++ stagingEnvironment/maint.tf
@@ -0,0 +1,19 @@
+terraform {
+   required_version = ">= 0.12"
+   required_providers {
+      azurerm = ">3.0"
+   }
+}
+
+provider "azurerm" {
+    features {
+      
+    }
+}
+
+module "webserverModule" {
+  source = "../webserverModule "
+  Resource_Group_name = "obab_Staging_"
+  location = "francecentral"
+  environment = "Staging"
+}
